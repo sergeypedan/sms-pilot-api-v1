cebd1be3977c56b9b13873f17c75422a28a9899a
Sergey Pedan <sergey.pedan@gmail.com>
2021-05-09 18:46:06 +0700

    Adds comments

--- lib/sms_pilot.rb
+++ lib/sms_pilot.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Module that for now encompasses only API client
+#
+module SmsPilot
+end
+
 require_relative "sms_pilot/version"
 require_relative "sms_pilot/errors"
 require_relative "sms_pilot/client"
